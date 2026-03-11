1ffd6354738833ccec9fccc7611fec394d69c185
satyajitmishra-dev <satyajitmishra791@gmail.com>
2026-03-11 13:15:51 +0530

    Update API URL for Render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: import.meta.env.PROD ? 'https://carepath-api.onrender.com/api' : '/api',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
